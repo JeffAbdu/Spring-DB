ce0c5441f07f10f929159ecb3b1473f5f2495f9e
JeffAbdu <abdenourdjafri@gmail.com>
2017-05-08 18:32:11 -0400

    Running a Query Against a MySQL Database
    - Witin the class "FoodGroupDAO":
    -> Add a property of type JdbcTemplate: private JdbcTemplate
    myJdbcTemplate;
    -> Generate a setter and getter for this property:
    
    public JdbcTemplate getMyJdbcTemplate() {
    return myJdbcTemplate;
    }
    
    public void setMyJdbcTemplate(JdbcTemplate myJdbcTemplate) {
    this.myJdbcTemplate = myJdbcTemplate;
    }
    
    ->  Change the setter by replacing the argument by "ds" of the type
    "DataSource" (import javax.sql.DataSource):
    the setter will be as the following:
    
    public void setMyJdbcTemplate(DataSource ds) {
    this.myJdbcTemplate = new JdbcTemplate(ds);
    }
    
    -> Since, we have only one bean of the type DataSource which is
    "myBasicDataSource", the we can autowire this method in order to pass
    that bean in the argument of the setter:
    Then, we Autowire the setter and we register our class: "FoodGroupDAO"
    as a bean as well with @Component:
    
    @Component("foodGroupDAO")
    public class FoodGroupDAO {...
    
    @Autowired
    public void setMyJdbcTemplate(DataSource ds) {
    this.myJdbcTemplate = new JdbcTemplate(ds);
    }
    
    ...}
    
    -> We scan our package within appContext.xml:
    
    <context:component-scan base-package="com.demo.model"/>
    
    - Within the method getFoodGroups() we perform the following:
    
    public List<FoodGroup> getFoodGroups(){
    return myJdbcTemplate.query("select * from foodgroups", new
    RowMapper<FoodGroup>(){
    
    });
    }
    
    -> RowMapper type: org.springframework.jdbc.core.RowMapper.
    -> Spring tool will complain as the method RowMapper<FoodGroup>() is not
    implemented.
    > click on Add unimplemented method, then Spring would help us by adding
    the following to the method RowMapper<FoodGroup>():
    
    public FoodGroup mapRow(ResultSet rs, int rowNum) throws SQLException {
    // TODO Auto-generated method stub
    return null;
    }
    > mapRow(ResultSet rs, int rowNum) method accesses to one row at the
    time.
    
    -> We implement the FoodGroup mapRow(ResultSet rs, int rowNum) by the
    following code by accessing one row at the time,
    and at the end myJdbcTemplate will return group of rows:
    
    FoodGroup fg = new FoodGroup();
    fg.setId(rs.getInt("id"));
    fg.setName(rs.getString("name"));
    fg.setDescription(rs.getString("description"));
    return fg;
    
    - In order to test our code, we create main method within myApp.java
    class:
    
    public static void main(String[] args) {
    
    ApplicationContext appContext = new
    FileSystemXmlApplicationContext("appContext.xml");
    FoodGroupDAO myFoodGroupDAO = appContext.getBean("foodGroupDAO",
    FoodGroupDAO.class);
    List<FoodGroup> myFoodGroupList = myFoodGroupDAO.getFoodGroups();
    for(FoodGroup fg: myFoodGroupList){
    System.out.println(fg.talkAboutYourself());
    }
    
    ((FileSystemXmlApplicationContext)appContext).close();
    }

--- src/main/java/com/demo/model/FoodGroupDAO.java
+++ src/main/java/com/demo/model/FoodGroupDAO.java
@@ -1,11 +1,47 @@
 package com.demo.model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+
+@Component("foodGroupDAO")
 public class FoodGroupDAO {
 
+	private JdbcTemplate myJdbcTemplate; 
+	
 	public List<FoodGroup> getFoodGroups(){
-		return null;
+		return myJdbcTemplate.query("select * from foodgroups", new RowMapper<FoodGroup>(){
+
+			public FoodGroup mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+              FoodGroup fg = new FoodGroup();
+              
+              fg.setId(rs.getInt("id"));
+              fg.setName(rs.getString("name"));
+              fg.setDescription(rs.getString("description"));
+              
+              return fg;
+				 
+			}
+			
+		});
+	}
+
+	public JdbcTemplate getMyJdbcTemplate() {
+		return myJdbcTemplate;
+	}
+
+	@Autowired
+	public void setMyJdbcTemplate(DataSource ds) {
+		this.myJdbcTemplate = new JdbcTemplate(ds);
 	} 
 	
 }
---
+++ src/main/java/myApp.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+import com.demo.model.FoodGroup;
+import com.demo.model.FoodGroupDAO;
+
+public class myApp {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext appContext = new FileSystemXmlApplicationContext("appContext.xml");
+		
+		FoodGroupDAO myFoodGroupDAO = appContext.getBean("foodGroupDAO", FoodGroupDAO.class);
+		
+		List<FoodGroup> myFoodGroupList = myFoodGroupDAO.getFoodGroups();		
+				
+
+		for(FoodGroup fg: myFoodGroupList){
+			System.out.println(fg.talkAboutYourself());
+		}
+		
+		((FileSystemXmlApplicationContext)appContext).close();
+		
+	}
+
+}
