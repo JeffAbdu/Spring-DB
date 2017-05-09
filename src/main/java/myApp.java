6d2746a85e54c2d579d4325fd9657b2c808dc4f4
JeffAbdu <abdenourdjafri@gmail.com>
2017-05-09 18:01:49 -0400

    Handling a Database Exception
    - Here we are going to look at the method "getFoodGroup" which returns
    the list of food group.
    - Let's say witnin query, we have a wrong spelling the table name like
    instead "foodGroups", we make it "foodGroup"
    -> As a result, the consule will throw exception, but witn not a nice
    way.
    -> Instead, we would handle this exception, but it is not good to handle
    it within FoodGroupDAO.
    > Instead, we will handle it within main() method class by adding the
    following exception handling:
    
    public class myApp {
    public static void main(String[] args) {
    ApplicationContext appContext = new
    FileSystemXmlApplicationContext("appContext.xml");
    try {
    FoodGroupDAO myFoodGroupDAO = appContext.getBean("foodGroupDAO",
    FoodGroupDAO.class);
    List<FoodGroup> myFoodGroupList = myFoodGroupDAO.getFoodGroups();
    for (FoodGroup fg : myFoodGroupList) {
    System.out.println(fg.talkAboutYourself());
    }
    
    } catch (PermissionDeniedDataAccessException e) {
    
    // Handle as you see fit
    
    } catch (DataAccessException e) {
    System.out.println(e.getMessage());
    System.out.println(e.getClass());
    }
    ((FileSystemXmlApplicationContext) appContext).close();
    }
    }
    
    -> PermissionDeniedDataAccessException is a subclass of
    DataAccessException.
    -> as a result, the consule would dispaly:
    
    PreparedStatementCallback; bad SQL grammar [select * from foodgroup
    where name=? ]; nested exception is
    com.mysql.jdbc.exceptions.jdbc4.MySQLSyntaxErrorException: Table
    'springtutorial.foodgroup' doesn't exist
    class org.springframework.jdbc.BadSqlGrammarException

--- src/main/java/myApp.java
+++ src/main/java/myApp.java
@@ -1,6 +1,10 @@
 
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.PermissionDeniedDataAccessException;
 
 import com.demo.model.FoodGroup;
 import com.demo.model.FoodGroupDAO;
@@ -8,17 +12,30 @@ import com.demo.model.FoodGroupDAO;
 public class myApp {
 
 	public static void main(String[] args) {
-		
+
 		ApplicationContext appContext = new FileSystemXmlApplicationContext("appContext.xml");
-		
-		FoodGroupDAO myFoodGroupDAO = appContext.getBean("foodGroupDAO", FoodGroupDAO.class);
-				
-		FoodGroup myFoodGroup = new FoodGroup(8,"aRandomGroupNameUpdated","aRandomGroupDescriptionUpdated");
-		
-		myFoodGroupDAO.delete(8);
-		
-		((FileSystemXmlApplicationContext)appContext).close();
-		
+
+		try {
+
+			FoodGroupDAO myFoodGroupDAO = appContext.getBean("foodGroupDAO", FoodGroupDAO.class);
+
+			List<FoodGroup> myFoodGroupList = myFoodGroupDAO.getFoodGroups();
+
+			for (FoodGroup fg : myFoodGroupList) {
+				System.out.println(fg.talkAboutYourself());
+			}
+
+		} catch (PermissionDeniedDataAccessException e) {
+
+			// Handle as you see fit
+
+		} catch (DataAccessException e) {
+			System.out.println(e.getMessage());
+			System.out.println(e.getClass());
+		}
+
+		((FileSystemXmlApplicationContext) appContext).close();
+
 	}
 
 }
